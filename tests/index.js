d8da4abf63e8da9f03123e01dd82ca4ba0c76554
İbrahim Telman <cheriorn@gmail.com>
2018-11-19 21:42:11 +0300

    Tests updated

--- tests/index.js
+++ tests/index.js
@@ -3,7 +3,7 @@ const generate_colors = require('../src/index');
 test('add generate color between white and black', () => {
     expect(JSON.stringify(generate_colors('#fff', '#000'))).toBe(JSON.stringify([
         "rgb(255,255,255)",
-        "rgb(127.5,127.5,127.5)",
+        "rgb(128,128,128)",
         "rgb(0,0,0)"
     ]))
 });
